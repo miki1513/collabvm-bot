63e8637d372d46c513c416dc9a19b846cd5cfc9d
imightexist <94249457+imightexist@users.noreply.github.com>
2022-02-04 11:58:06 -0600

    change username

--- bot.js
+++ bot.js
@@ -1,8 +1,16 @@
 var latestMessage;
+
+//change username
+document.getElementById("username-btn").click();
+document.getElementById("username-box").value = "cvmbot";
+document.getElementById("username-ok-btn").click();
+
 function send(string) {
   document.getElementById("chat-input").value = string; //inputs string to input box
   document.getElementById("chat-send-btn").click(); //clicks send button
 }
+
+//checks for latest message
 while (true) {
   latestMessage = document.getElementById("chat-box").lastElementChild.lastElementChild.textContent.split('▸')[1]; //gets everything after ▸ from latest message on chatbox
   if (latest-message == "!test") {
