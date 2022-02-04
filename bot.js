b560d772215648f16bd10cdb12b85afa54f499f3
imightexist <94249457+imightexist@users.noreply.github.com>
2022-02-04 12:57:27 -0600

    new commands

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 var latestMessage;
+var latestSender;
 
 //change username
 document.getElementById("username-btn").click();
@@ -12,12 +13,30 @@ function send(string) {
 
 //checks for latest message
 while (true) {
-  latestMessage = document.getElementById("chat-box").lastElementChild.lastElementChild.textContent.split('▸')[1]; //gets everything after ▸ from latest message on chatbox
+  latestMessage = document.getElementById("chat-box").lastElementChild.lastElementChild.textContent.split('▸')[1]; //gets everything after ▸ aka the message itself
+  latestSender = document.getElementById("chat-box").lastElementChild.lastElementChild.textContent.split('▸')[0]; //gets everything before ▸ aka the sender
   if (latestMessage == "!test") {
     send("hi");
   }
   if (latestMessage == "!help") {
     send("!test - responds hi");
     send("!help - shows commands");
+    send("!about - about bot");
+    send("!mtgen - from general darian");
+    send("!say - says whatever is after it");
+    send("!selfdestruct - kills bot");
+  }
+  if (latestMessage == "!about") {
+    send("CollabVM-Bot Beta");
+  }
+  if (latestMessage == "!mtgen") {
+    send(latestSender + ", no");
+  }
+  if (latestMessage.includes("!say")) {
+    send(latestMessage.replace('!say ',''));
+  }
+  if (latestMessage == "!selfdestruct") {
+    send("goodbye world");
+    window.close();
   }
 }
