93f5ee9e3f9a84c4911ebd5fa03db03dfe23127a
imightexist <94249457+imightexist@users.noreply.github.com>
2022-02-27 13:17:46 -0600

    Update nodejs.js

--- nodejs.js
+++ nodejs.js
@@ -8,22 +8,44 @@ function connect(){
     function send(string){
       f.sendUTF(encodeCommand(['chat', string]));
     }
-    function username(string){
+    function changeUsername(string){
       f.sendUTF('6.rename,' + string.length + '.' + string + ';');
     }
-    username("CollabVM Bot c!help");
     f.on('message',function(message){
       var cmd = decodeCommand(message.utf8Data);
+      changeUsername("CollabVM Bot c!help");
       var username = cmd[1];
       var command = cmd[2];
       var prefix = "c!";
-      var args = command.slice(prefix.length).split(' ');
       
       //now here is the fun part
       if (command == prefix + "test"){
-        send("hello world")
+        send("hello world");
       }
-    }
-  }
+    })
+  })
   ws.connect('ws://' + vm, 'guacamole');
 }
+function decodeCommand(cypher) {
+	var sections = [];
+	var bump = 0;
+	while (sections.length <= 50 && cypher.length >= bump) {
+		var current = cypher.substring(bump);
+		var length = parseInt(current.substring(current.search(/\./) - 2));
+		var paramater = current.substring(length.toString().length + 1, Math.floor(length / 10) + 2 + length);
+		sections[sections.length] = paramater;
+		bump += Math.floor(length / 10) + 3 + length;
+	}
+	sections[sections.length - 1] = sections[sections.length - 1].substring(0, sections[sections.length - 1].length - 1);
+	return sections;
+}
+function encodeCommand(cypher) {
+	var command = "";
+	for (var i = 0; i < cypher.length; i++) {
+		var current = cypher[i];
+		command += current.length + "." + current;
+		command += (i < cypher.length - 1 ? "," : ";");
+	}
+	return command;
+}
+connect();
